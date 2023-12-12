d1d597e207854710ad5963e9eff72aa32785cb8b
muo-ahn <dksehdgur321@gmail.com>
2023-12-12 13:51:19 +0900

    ai ml traning on web

--- NodeJS/ai.js
+++ NodeJS/ai.js
@@ -2,6 +2,8 @@ var express = require("express");
 var router = express.Router();
 var template = require("./template.js");
 
+const modulePath = "./NodeJS/AI_Module/test.py";
+
 router.get("", function (req, res) {
   var title = "ai 모듈 테스트";
   var html = template.HTML(
@@ -20,10 +22,10 @@ router.get("", function (req, res) {
 router.post("/ai_process", function (req, res) {
   let text = "";
   //1. child-process 모듈의 spawn
-  const { spawn } = require("child_process");
+  var { spawn } = require("child_process");
 
   //2. spawn을 통해 python filename.py 명령어 실행
-  const result = spawn("python", ["./NodeJS/AI_Module/helloworld.py"]);
+  var result = spawn("python", [modulePath]);
   result.stdout.on("data", function (data) {
     text = data.toString();
 
