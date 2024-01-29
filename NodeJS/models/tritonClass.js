664373d0965f2901be602a05f8f74c1b3a3ea551
muo-ahn <dksehdgur321@gmail.com>
2024-01-29 20:50:36 +0900

    update Jan 29

--- NodeJS/models/tritonClass.js
+++ NodeJS/models/tritonClass.js
@@ -143,7 +143,9 @@ function parseResult(postData) {
           }
           break;
         case "eye":
-          detectedDisease.add(DiseaseTable.getDiseaseByIndex(4, indexOffset));
+          if (data.modelIndex == 1) {
+            detectedDisease.add(DiseaseTable.getDiseaseByIndex(4, indexOffset));
+          }
           break;
         default:
           detectedDisease.add("nor");
@@ -177,7 +179,9 @@ function getDiseaseID(modelName) {
     Ab09: "abdominal\x079",
     Ch01: "thoracic\x071",
     Ch02: "thoracic\x072",
-    Ey08: "eye\x078",
+    cat_Ey07: "eye\x077",
+    cat_Ey08: "eye\x078",
+    cat_Ey10: "eye\x010",
   };
 
   return diseaseMap[modelName] || "error";
@@ -188,13 +192,24 @@ function ModelSelect(petbreed, api) {
   const petModels = {
     dog: {
       skin: ["skin_dog"],
-      eye: ["Ey08"],
+      eye: [
+        "dog_Ey01",
+        "dog_Ey02",
+        "dog_Ey03",
+        "dog_Ey04",
+        "dog_Ey05",
+        "dog_Ey06",
+        "dog_Ey07",
+        "dog_Ey08",
+        "dog_Ey10",
+        "dog_Ey11",
+      ],
       abdominal: ["Ab01", "Ab04", "Ab05", "Ab09"],
       thoarcic: ["Ch01", "Ch02"],
     },
     cat: {
       skin: ["skin_cat"],
-      eye: ["Ey08"],
+      eye: ["cat_Ey07", "cat_Ey08", "cat_Ey10"],
       abdominal: ["Ab01", "Ab04", "Ab05", "Ab09"],
       thoarcic: ["Ch01", "Ch02"],
     },
